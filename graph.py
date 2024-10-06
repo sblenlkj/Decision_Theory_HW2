9b9adfa76f5a0dcadcd4684eb14170a49745da0d
blenlkj <sblenlkj@gmail.com>
2024-10-06 14:46:30 +0300

    parse GetData is simple class now, graph MyGraph process_final_json method, data is uted, looked through, 2024_final.json is created with connections and nodes k value parts

---
+++ graph.py
@@ -0,0 +1,65 @@
+import json
+import hashlib
+from collections import defaultdict, Counter
+from itertools import combinations
+
+class Connection:
+    def __init__(self, a, b):
+        self.a, self.b = sorted([a, b])
+    
+    def __repr__(self) -> str:
+        return f"Connection({self.a}, {self.b})"
+    
+    def __eq__(self, value: object) -> bool:
+        return self.a == value.a and self.b == value.b
+    
+    def __hash__(self) -> int:
+        return self.hash_function(self.b + self.a)
+    
+    def to_list(self) -> list:
+        return [self.a, self.b]
+    
+    def hash_function(self, s):
+        return int(hashlib.sha1(s.encode("utf-8")).hexdigest(), 16) % (10 ** 8)
+
+
+class MyGraph:
+    folder_path = "data/"
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def process_final_json(self, to_save=False) -> dict:
+        infile = open(f"{self.folder_path}{self.file_name}_united.json")
+        dct = json.load(infile)
+        
+        new_dct = defaultdict(list)
+        
+        for v in dct["data"].values():
+            nodes = []
+            new_dct["tutors"].append(v["tutor"])
+            nodes.append(v["tutor"])
+
+            new_dct["countries"].extend(v["countries"])
+            nodes.extend(v["countries"])
+
+            new_dct["topics"].extend(v["topics"])
+            nodes.extend(v["topics"])
+
+            new_dct["connections"].extend([Connection(t[0], t[1]) for t in combinations(nodes, 2)])
+        
+        for k, v in new_dct.items():
+            dct[k] = Counter(v)
+            
+            if k != "connections":
+
+                dct[k] = list(map(lambda x: {x[0]: x[1]}, sorted(dct[k].items())))
+        
+
+        dct["connections"] = [{"times": v, "nodes": c.to_list()} for c, v in dct["connections"].items()]
+
+        if to_save:
+            outfile = open(f"{self.folder_path}{self.file_name}_final.json", "w")
+            json.dump(dct, outfile, indent=4)
+        
+        return dct
--- parse.py
+++ parse.py
@@ -6,32 +6,34 @@ from deep_translator import GoogleTranslator
 import logging
 logging.basicConfig(level=logging.INFO)
 
-handle = "parse"
-logger = logging.getLogger(handle)
 
 class GetData:
     folder_path = "data/"
     translator = GoogleTranslator(source='ru', target='en')
+    logger = logging.getLogger("parse")
 
-    @classmethod
-    def get_data(cls, url: str, file_name: str):
+    def __init__(self, file_name: str):
+        self.file_name = file_name
+
+    
+    def get_data(self, url: str):
         response = requests.get(url)
         soup = BeautifulSoup(response.content, 'html.parser')
 
         data = []
-        data.extend(cls.extract_data(soup))
-        last_page_number = cls.get_last_page_number(soup)
+        data.extend(self.extract_data(soup))
+        last_page_number = self.get_last_page_number(soup)
         
-        for i in range(2, last_page_number+1):
-            logger.info(f"page №{i}")
+        # for i in range(2, last_page_number+1):
+        #     self.logger.info(f"page №{i}")
 
-            new_url = f"{url}&page={i}"
-            response = requests.get(new_url)
-            soup = BeautifulSoup(response.content, 'html.parser')
+        #     new_url = f"{url}&page={i}"
+        #     response = requests.get(new_url)
+        #     soup = BeautifulSoup(response.content, 'html.parser')
 
-            data.extend(cls.extract_data(soup))
+        #     data.extend(self.extract_data(soup))
         
-        cls.save_to_file(data, file_name)
+        self.save_to_file(data)
 
 
     @staticmethod
@@ -39,53 +41,57 @@ class GetData:
         return int(soup.find("div", {"class": "letterlist"}).find_all("a")[-1].text.strip())
         
 
-    @classmethod
-    def extract_data(cls, soup: BeautifulSoup, translate_flag: bool=True):
+    def extract_data(self, soup: BeautifulSoup, translate_flag: bool=True):
         blocks = soup.find_all("li", {"class": "vkr-card vkr-list__item"})
         lst = []
 
         for i, block in enumerate(blocks):
-            logger.info(i)
+            self.logger.info(i)
 
             topic = block.find("h3").text.strip()
             if translate_flag:
-                topic = cls.translator.translate(topic)
+                topic = self.translator.translate(topic)
             
             tutor = block.find_all("p")[3].find("span").text.strip().split()[0]
             if translate_flag:
-                tutor = cls.translator.translate(tutor)
+                tutor = self.translator.translate(tutor)
             
             lst.append([topic, tutor]) 
         
         return lst
 
-    @classmethod
-    def save_to_file(cls, data: dict, file_name: str):
-        with open(f"{cls.folder_path}{file_name}.txt", "w") as outfile:
+    def save_to_file(self, data: dict):
+        with open(f"{self.folder_path}{self.file_name}.txt", "w") as outfile:
             for i, (topic, _) in enumerate(data):
                 print(f"{i+1}. {topic}", file=outfile)
         
         dct = {"data": {i+1: {"topic": topic, "tutor": tutor} for i, (topic, tutor) in enumerate(data)}}
-        outfile = open(f"{cls.folder_path}{file_name}_raw.json", "w")
+        outfile = open(f"{self.folder_path}{self.file_name}_raw.json", "w")
 
         json.dump(dct, outfile, indent=4)
     
-    @classmethod
-    def unite_raw_ollama_jsons(cls, file_name: str):
-        infile_raw = open(f"{cls.folder_path}{file_name}_raw.json")
+
+    def unite_raw_ollama_jsons(self):
+        infile_raw = open(f"{self.folder_path}{self.file_name}_raw.json")
         dct_raw = json.load(infile_raw)
 
-        infile_ollama = open(f"{cls.folder_path}{file_name}_ollama.json")
+        infile_ollama = open(f"{self.folder_path}{self.file_name}_ollama.json")
         dct_ollama = json.load(infile_ollama)
 
         for k, v in dct_ollama.items():
-            dct_raw["data"][k]["topics"] = v
+            dct_raw["data"][k].update(v)
         
-        outfile = open(f"{cls.folder_path}{file_name}_united.json", "w")
+        outfile = open(f"{self.folder_path}{self.file_name}_united.json", "w")
         json.dump(dct_raw, outfile, indent=4)
 
+def main():
+    url, file_name = "https://www.hse.ru/edu/vkr/?year=2024&program=p135181773%3Bg122468442", "test"
+    g = GetData(file_name)
+    
+    g.get_data(url)
+    # g.unite_raw_ollama_jsons()
 
-# url = "https://www.hse.ru/edu/vkr/?year=2024&program=p135181773%3Bg122468442"
-# GetData.get_data(url, "2024")
 
-GetData.unite_raw_ollama_jsons("2024")
+if __name__ == "__main__":
+    main()
+
